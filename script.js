e2c9af83659ab037f51ae1c92040e0f4e37b405f
sujipinku07 <sujipinku07@gmail.com>
2024-09-28 20:46:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+ function updateWindowSize() {
+        const sizeInfoDiv = document.getElementById("sizeInfo");
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+
+        sizeInfoDiv.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
+      }
+
+      // Initial call to display the size when the page is first loaded
+      updateWindowSize();
+
+      // Update the size when the window is resized
+      window.addEventListener("resize", updateWindowSize);//your JS code here. If required.
